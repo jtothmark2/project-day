78e566ab7458cb08fb241f579b7367d935c7e768
kokasmark <mark.kokas04@gmail.com>
2024-04-17 12:15:26 +0200

    Getting movies done. Movie cards redesigned

--- frontend/clapp/src/Movies.js
+++ frontend/clapp/src/Movies.js
@@ -2,10 +2,19 @@ import './App.css';
 import './Movies.css';
 import { Component, React } from 'react';
 import { ReactComponent as CloseCircle } from './assets/close-circle.svg';
-
+import { Call } from './api';
 class Movies extends Component {
   state = {
-    selectedMovie: null
+    selectedMovie: null,
+    movies: []
+  }
+  componentDidMount(){
+    this.GetMovies();
+  }
+  async GetMovies(){
+    var r = await Call('GET', 'api/movies', {})
+    console.log(r)
+    this.setState({movies: r})
   }
   render() {
     return (
@@ -15,101 +24,29 @@ class Movies extends Component {
         <h1 className='moviesHeader' >Our next movies:</h1>
 
         <div className='movies'>
-          <div className='movieCard'>
+          {
+            this.state.movies.map((movie,index) => (
+              <div key={index} className='movieCard'>
 
-            <img src={require("./assets/cinema.jpg")} className='movieImg'></img>
+            <img src={require(`./assets${movie.img_url}`)} className='movieImg'></img>
 
 
-            <h3 className='movieTitle'>Movie title</h3>
+            <h3 className='movieTitle'>{movie.title}</h3>
             <div className='movieData'>
-              <p> genre - genre</p>
-              <p>120 min.</p>
+              <p>{movie.genre}</p>
+              <p>{movie.length}</p>
             </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+            <p className='desc'>
+              {movie.description}
             </p>
             <div className='btn'>
               <button onClick={() => this.setState({ selectedMovie: { title: "superman", rating: 8.5 } })} className='detailsBtn'>Details</button>
             </div>
 
           </div>
-
-          {/* sdas */}
-
-          <div className='movieCard'>
-
-            <img src={require("./assets/cinema.jpg")} className='movieImg'></img>
-
-
-            <h3 className='movieTitle'>Movie title</h3>
-            <div className='movieData'>
-              <p> genre - genre</p>
-              <p>120 min.</p>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
-            </p>
-            <div className='btn'>
-              <button onClick={() => this.setState({ selectedMovie: { title: "superman", rating: 8.5 } })} className='detailsBtn'>Details</button>
-            </div>
-
-          </div>
-          <div className='movieCard'>
-
-
-            <img src={require("./assets/cinema.jpg")} className='movieImg'></img>
-
-
-            <h3 className='movieTitle'>Movie title</h3>
-            <div className='movieData'>
-              <p> genre - genre</p>
-              <p>120 min.</p>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
-            </p>
-            <div className='btn'>
-              <button onClick={() => this.setState({ selectedMovie: { title: "superman", rating: 8.5 } })} className='detailsBtn'>Details</button>
-            </div>
-
-          </div>
-          <div className='movieCard'>
-
-            <img src={require("./assets/cinema.jpg")} className='movieImg'></img>
-
-
-            <h3 className='movieTitle'>Movie title</h3>
-            <div className='movieData'>
-              <p> genre - genre</p>
-              <p>120 min.</p>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
-            </p>
-            <div className='btn'>
-              <button onClick={() => this.setState({ selectedMovie: { title: "superman", rating: 8.5 } })} className='detailsBtn'>Details</button>
-            </div>
-
-          </div>
-          <div className='movieCard'>
-
-            <img src={require("./assets/cinema.jpg")} className='movieImg'></img>
-
-
-            <h3 className='movieTitle'>Movie title</h3>
-            <div className='movieData'>
-              <p> genre - genre</p>
-              <p>120 min.</p>
-            </div>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
-            </p>
-            <div className='btn'>
-              <button onClick={() => this.setState({ selectedMovie: { title: "superman", rating: 8.5 } })} className='detailsBtn'>Details</button>
-            </div>
-
-          </div>
-          {/* sad */}
+            ))
+          }
+          
         </div>
 
 
