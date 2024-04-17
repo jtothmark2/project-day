bbcfd09c3dd3016713f0ed29154edd6b1fb21202
kokasmark <mark.kokas04@gmail.com>
2024-04-17 11:45:57 +0200

    Api.js Done.

---
+++ frontend/clapp/src/api.js
@@ -0,0 +1,30 @@
+const ip = "10.0.22.16:3001";
+
+
+export async function Call(rest,route, args){
+    var requestOptions = {};
+    var myHeaders = new Headers();
+    var raw = JSON.stringify(args);
+    if(rest === "GET"){
+         requestOptions = {
+            method: "GET",
+            redirect: "follow"
+          }; 
+    }
+    if(rest === "POST"){
+        requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+          };
+   }
+   try {
+    const response = await fetch(`http://${ip}/${route}`, requestOptions);
+    const data = await response.json();
+    return data; // Return the data from the API call
+} catch (error) {
+    throw error; // Throw any errors that occur during the API call
+}
+}
+ 
