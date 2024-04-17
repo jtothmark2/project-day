709dd6b479b458cc006cae94b94ac60b4f633dec
dezbence <dezbence@gmail.com>
2024-04-17 15:04:29 +0200

    final order

--- frontend/clapp/src/Order.js
+++ frontend/clapp/src/Order.js
@@ -1,12 +1,16 @@
 import './Order.css';
 import { Component, React } from 'react';
+import QRCode from 'react-qr-code';
+
+
 
 
 
 class Order extends Component {
 
   state = {
-    order: []
+    order: [],
+    value: "https://www.youtube.com/watch?v=xvFZjo5PgG0",
   }
   // componentDidMount(){
   //   this.GetFoods();
@@ -29,7 +33,17 @@ class Order extends Component {
               <h1 className='name'>Kis Levente - 2022.12.01</h1>
             </div>
             <div className='orderDetails'>
-              <div className='qr'>qr kód</div>
+              <div className='qr'>
+              {this.state.value && (
+                    <QRCode
+                        title="Clapp"
+                        value={this.state.value}
+                        bgColor='#FFFFFF'
+                        fgColor='#000000'
+                        size="256"
+                    />
+                )}
+              </div>
               <ul className='orders'>
                 <li>sadasdasd</li>
                 <li>sadasdasd</li>
