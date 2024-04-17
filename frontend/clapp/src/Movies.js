82034c53046a9db119acde1a927061397d3d7171
nyikoskata <nykata04@gmail.com>
2024-04-17 10:53:54 +0200

    big movie card

--- frontend/clapp/src/Movies.js
+++ frontend/clapp/src/Movies.js
@@ -1,39 +1,139 @@
 import './App.css';
 import './Movies.css';
-import {Component, React} from 'react';
+import { Component, React } from 'react';
 
 class Movies extends Component {
   state = {
-    selectedMovie: {title: "superman", rating: 8.5}
+    selectedMovie: null
   }
-  render(){
+  render() {
     return (
       <div className="App">
-       <h1>Movies</h1>
+        <h1>Movies</h1>
 
-       <h1 className='moviesHeader' >Our next movies:</h1>
+        <h1 className='moviesHeader' >Our next movies:</h1>
 
-       <div className='movieCard'>
+        <div className='movies'>
+          <div className='movieCard'>
 
-          <img src={require("./cinema.jpg")} className='movieImg'></img>
+            <img src={require("./cinema.jpg")} className='movieImg'></img>
 
 
-          <h3>Movie title</h3>
-          <div className='movieData'>
-            <p> genre - genre</p>
-            <p>120 min.</p>
+            <h3 className='movieTitle'>Movie title</h3>
+            <div className='movieData'>
+              <p> genre - genre</p>
+              <p>120 min.</p>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+            </p>
+            <div className='btn'>
+              <button onClick={() => this.setState({ selectedMovie: { title: "superman", rating: 8.5 } })} className='detailsBtn'>Details</button>
+            </div>
+
           </div>
-          <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
-          </p>
-          <div className='btn'>
-            <button onClick={()=>this.setState({selectedMovie: {title: "superman", rating: 8.5}})} className='detailsBtn'>Details</button>
+
+          {/* sdas */}
+
+          <div className='movieCard'>
+
+            <img src={require("./cinema.jpg")} className='movieImg'></img>
+
+
+            <h3 className='movieTitle'>Movie title</h3>
+            <div className='movieData'>
+              <p> genre - genre</p>
+              <p>120 min.</p>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+            </p>
+            <div className='btn'>
+              <button onClick={() => this.setState({ selectedMovie: { title: "superman", rating: 8.5 } })} className='detailsBtn'>Details</button>
+            </div>
+
           </div>
+          <div className='movieCard'>
 
-       </div>
+            <img src={require("./cinema.jpg")} className='movieImg'></img>
 
-        {this.state.selectedMovie != null && 
-          <div>
+
+            <h3 className='movieTitle'>Movie title</h3>
+            <div className='movieData'>
+              <p> genre - genre</p>
+              <p>120 min.</p>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+            </p>
+            <div className='btn'>
+              <button onClick={() => this.setState({ selectedMovie: { title: "superman", rating: 8.5 } })} className='detailsBtn'>Details</button>
+            </div>
+
+          </div>
+          <div className='movieCard'>
+
+            <img src={require("./cinema.jpg")} className='movieImg'></img>
+
+
+            <h3 className='movieTitle'>Movie title</h3>
+            <div className='movieData'>
+              <p> genre - genre</p>
+              <p>120 min.</p>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+            </p>
+            <div className='btn'>
+              <button onClick={() => this.setState({ selectedMovie: { title: "superman", rating: 8.5 } })} className='detailsBtn'>Details</button>
+            </div>
+
+          </div>
+          <div className='movieCard'>
+
+            <img src={require("./cinema.jpg")} className='movieImg'></img>
+
+
+            <h3 className='movieTitle'>Movie title</h3>
+            <div className='movieData'>
+              <p> genre - genre</p>
+              <p>120 min.</p>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+            </p>
+            <div className='btn'>
+              <button onClick={() => this.setState({ selectedMovie: { title: "superman", rating: 8.5 } })} className='detailsBtn'>Buy ticket</button>
+            </div>
+
+          </div>
+          {/* sad */}
+        </div>
+
+
+
+        {this.state.selectedMovie != null &&
+
+
+          <div className='showDetails'>
+
+            <div className='bigMovieCard'>
+
+              <img src={require("./cinema.jpg")} className='bigMovieImg'></img>
+
+              <h3 className='movieTitle'>Movie title</h3>
+              <div className='movieData'>
+                <p> genre - genre</p>
+                <p>120 min.</p>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.
+              </p>
+              <div className='btn'>
+                <button className='detailsBtn'>Foglalás</button>
+              </div>
+
+            </div>
 
           </div>}
       </div>
